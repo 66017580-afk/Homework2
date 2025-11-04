5ce66837d216c65d00cf8035d9a3111da0974066
66017580-afk <66017580@rbac.ac.th>
2025-11-04 12:42:38 +0700

    commit2

---
+++ menu.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Menu() {
+  return (
+    <nav>
+      <a href="#">หน้าแรก</a> |{" "}
+      <a href="#">เกี่ยวกับ</a> |{" "}
+      <a href="#">ติดต่อ</a>
+    </nav>
+  );
+}
+
+export default Menu;
---
+++ product.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Product() {
+  return (
+    <div>
+      <h2>สินค้า</h2>
+      <ul>
+        <li>โทรศัพท์มือถือ - 10,000 บาท</li>
+        <li>โน้ตบุ๊ก - 25,000 บาท</li>
+        <li>หูฟัง - 1,200 บาท</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Product;
